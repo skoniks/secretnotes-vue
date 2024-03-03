edae16972816454ae1fa082056d9d440ab154d48
skoniks <skolya16@ya.ru>
2024-03-03 09:42:00 +0300

    Update template base

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+//
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <span style="color: brown">
+    <FontAwesomeIcon :icon="faUserSecret" />
+  </span>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique delectus
+    quaerat aperiam, illo est repellendus. Quo ad earum quisquam minus,
+    excepturi voluptas praesentium laudantium adipisci officiis quibusdam
+    dolores dolor saepe.
+  </p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style scoped lang="scss">
+p {
+  font-size: 1.5em;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import './assets/main.css'
+import '@fontsource/nunito';
+import './assets/main.scss';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
